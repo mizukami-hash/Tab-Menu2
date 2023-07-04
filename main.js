cbb69eb2e83bc672247db2c4fd9f67adc5786a3d
mizukami-hash <128657673+mizukami-hash@users.noreply.github.com>
2023-07-04 18:56:26 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
+
+
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+        // aタグのページ遷移を無効に
+      e.preventDefault();
+
+      menuItems.forEach((item)=>{
+        item.classList.remove("active");
+      });
+      clickedItem.classList.add('active');
+
+
+      contents.forEach(content=>{
+        content.classList.remove("active");
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add("active");
+    });
+  });
+}
